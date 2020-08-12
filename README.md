c35587776508594a0570dae86aa4548b0dd59e13
Lucas Yang <yangchenshin77@gmail.com>
2020-08-13 01:28:43 +0800

    Modified to lucas preset

--- README.md
+++ README.md
@@ -1,21 +1,18 @@
 <p align="center">
-  <h1 align="center">Laravel Pest</h1>
+  <h1 align="center">Laravel Pest - Lucas</h1>
   <p align="center">
-    <a href="https://github.com/use-preset/use-preset/releases">
-      <img alt="npx use-preset use-preset/Laravel Pest" src="https://img.shields.io/badge/use--preset-laravel--pest-blue?style=flat-square">
-    </a>
+    <a href="https://github.com/use-preset/use-preset/releases"><img alt="npx use-preset ycs77/preset-laravel-pest" src="https://img.shields.io/badge/use--preset-laravel--pest-blue?style=flat-square"></a>
     &nbsp;
-    <a href="https://www.npmjs.com/package/use-preset">
-      <img alt="use-preset version" src="https://img.shields.io/npm/v/use-preset?color=32c854&style=flat-square&label=use-preset">
-    </a>
+    <a href="https://www.npmjs.com/package/use-preset"><img alt="use-preset version" src="https://img.shields.io/npm/v/use-preset?color=32c854&style=flat-square&label=use-preset"></a>
   </p>
   <br />
   <p align="center">
     <code>use-preset</code> is a scaffolding tool for developers. <a href="https://docs.usepreset.dev/">Read the documentation</a> for more information.
   </p>
   <br />
-  <pre align="center">npx use-preset laravel-pest</pre>
-  &nbsp;
+  <pre align="center">npx use-preset ycs77/preset-laravel-pest</pre>
+  <br />
+  <p align="center">See more <a href="https://github.com/ycs77/presets-list">Lucas's Presets</a></p>
 <p>
 
 # About
@@ -27,12 +24,20 @@ This preset adds [Pest](https://pestphp.com) to a fresh Laravel application.
 **Run the following command**:
 
 ```bash
-npx use-preset laravel-pest
+npx use-preset ycs77/preset-laravel-pest
+```
+
+# Usage
+
+Run the test using Pest:
+
+```bash
+composer run test
 ```
 
 # Modifications
 
-This preset follows the [installation instructions](https://pestphp.com/docs/installation/) of Pest by adding `pestphp/pest` and `pestphp/pest-plugin-laravel`, and updating `nunomaduro/collision` and `phpunit/phpunit` to the correct versions.
+This preset follows the [installation instructions](https://pestphp.com/docs/installation/) of Pest by adding `pestphp/pest` and `pestphp/pest-plugin-laravel`, and updating `nunomaduro/collision` and `phpunit/phpunit` to the correct versions. This version add the example test case file for Pest.
 
 It also adds a `test` script to `composer.json` to run `.\vendor\bin\pest`.
 
--- src/preset.js
+++ src/preset.js
@@ -1,48 +1,50 @@
-const { Preset } = require('use-preset');
+const { Preset } = require('use-preset')
 
 // prettier-ignore
 module.exports = Preset.make('Laravel Pest')
-	.option('interaction', true)
-	.option('install', true)
+  .option('interaction', true)
+  .option('install', true)
 
-	.editJson('composer.json')
-		.title('Add Pest and its dependencies')
-		.delete(['require-dev.phpunit/phpunit', 'require-dev.nunomaduro/collision'])
-		.merge({
-			'minimum-stability': 'dev',
-			'prefer-stable': true,
-			'require-dev': {
-				'nunomaduro/collision': '^5.0',
-				'phpunit/phpunit': '^9.0',
-				'pestphp/pest': '^0.2.4',
-				'pestphp/pest-plugin-laravel': '^0.2',
-			},
-			scripts: {
-				test: '.\\vendor\\bin\\pest'
-			}
-		})
-		.chain()
+  .editJson('composer.json')
+    .title('Add Pest and its dependencies')
+    .delete(['require-dev.phpunit/phpunit', 'require-dev.nunomaduro/collision'])
+    .merge({
+      'minimum-stability': 'dev',
+      'prefer-stable': true,
+      'require-dev': {
+        'nunomaduro/collision': '^5.0',
+        'phpunit/phpunit': '^9.0',
+        'pestphp/pest': '^0.2.4',
+        'pestphp/pest-plugin-laravel': '^0.2',
+      },
+      scripts: {
+        test: '.\\vendor\\bin\\pest'
+      }
+    })
+    .chain()
 
-	.updateDependencies()
-		.withoutAsking(({ flags }) => Boolean(flags.interaction) && Boolean(flags.install))
-		.for('php')
-		.chain()
+  .updateDependencies()
+    .withoutAsking(({ flags }) => Boolean(flags.interaction) && Boolean(flags.install))
+    .for('php')
+    .chain()
 
-	// Installs Pest. The code is weird, because there is an issue until Pest v0.3
-	// where it is not listening to --no-interaction.
-	.command('php', ['artisan', 'pest:install', '--no-interaction'])
-		.title('Install Pest')
-		.withOptions({ stdio: process.platform === 'linux' ? 'ignore' : 'pipe' })
-		.withHook(() => (child) => {
-			// Can't get this to work with Linux, because I lack understanding
-			// of how it works
-			if (process.platform === 'linux') {
-				return;
-			}
+  // Installs Pest. The code is weird, because there is an issue until Pest v0.3
+  // where it is not listening to --no-interaction.
+  .command('php', ['artisan', 'pest:install', '--no-interaction'])
+    .title('Install Pest')
+    .withOptions({ stdio: process.platform === 'linux' ? 'ignore' : 'pipe' })
+    .withHook(() => (child) => {
+      // Can't get this to work with Linux, because I lack understanding
+      // of how it works
+      if (process.platform === 'linux') {
+        return
+      }
 
-			// Needed until Pest v0.3, because it will not listen to
-			// --no-interaction until then
-			child.stdin.write('no\r\n');
-			child.stdin.end();
-		})
-		.chain();
+      // Needed until Pest v0.3, because it will not listen to
+      // --no-interaction until then
+      child.stdin.write('no\r\n')
+      child.stdin.end()
+    })
+    .chain()
+
+  .copyTemplates()
